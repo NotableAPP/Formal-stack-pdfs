f3930694b78a08525bf2fdb4d810f223e7c856c4
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-20 18:59:07 +0530

    Update sw.js

--- APP/sw.js
+++ APP/sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open('fs_bst_pdf').then(function(cache) {
+      caches.open('fs_bst_pdf_vone').then(function(cache) {
         return cache.addAll([
-          // add app with domain
+          "https://formal-stack.netlify.app",
           "index.html",
           "assets/done_black_24dp.svg",
           "assets/favicon.png",
