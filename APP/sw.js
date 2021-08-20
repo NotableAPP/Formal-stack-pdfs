477abf41a6205c070998429159616e0a9b596547
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-20 19:08:41 +0530

    Update sw.js

--- APP/sw.js
+++ APP/sw.js
@@ -2,28 +2,7 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('fs_bst_pdf_vone').then(function(cache) {
         return cache.addAll([
-          "https://formal-stack.netlify.app",
-          "index.html",
-          "assets/done_black_24dp.svg",
-          "assets/favicon.png",
-          "assets/ico@192.png",
-          "assets/img_add.svg",
-          "assets/watermark.png",
-          "assets/watermark64",
-          "libs/cropper.min.css",
-          "libs/cropper.min.js",
-          "libs/Google-mt-fonts.woff2",
-          "libs/jspdf.umd.min.js",
-          "libs/material.min (1).css",
-          "libs/material.min.js",
-          "libs/TeDlert.css",
-          "libs/TeDlertmain.js",
-          "pages/create.html",
-          "pages/settings.html",
-          "create.js",
-          "desktop.css",
-          "style.css",
-          "ui.js"
+            "index.html"        
         ]);
       })
     );
