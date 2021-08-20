2c350a32f34e017f6f5f5200675ab5e626fbc95f
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-20 21:31:27 +0530

    Added sw.js cache valid

--- APP/sw.js
+++ APP/sw.js
@@ -2,28 +2,8 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('fs_bst_pdf').then(function(cache) {
         return cache.addAll([
-          // add app with domain
-          "index.html",
-          "assets/done_black_24dp.svg",
-          "assets/favicon.png",
-          "assets/ico@192.png",
-          "assets/img_add.svg",
-          "assets/watermark.png",
-          "assets/watermark64",
-          "/APP/libs/cropper.min.css",
-          "/APP/libs/cropper.min.js",
-          "/APP/libs/Google-mt-fonts.woff2",
-          "/APP/libs/jspdf.umd.min.js",
-          "/APP/libs/material.min (1).css",
-          "/APP/libs/material.min.js",
-          "/APP/libs/TeDlert.css",
-          "/APP/libs/TeDlertmain.js",
-          "/APP/pages/create.html",
-          "/APP/pages/settings.html",
-          "/APP/create.js",
-          "/APP/desktop.css",
-          "/APP/style.css",
-          "/APP/ui.js"
+          "https://formal-stack.netlify.app/APP/",
+          "index.html"
         ]);
       })
     );
