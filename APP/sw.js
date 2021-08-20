b75bfb01b9a5ff7458897cc3be87a10ccaffde22
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-20 19:13:56 +0530

    Add files via upload

--- APP/sw.js
+++ APP/sw.js
@@ -1,8 +1,30 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open('fs_bst_pdf_vone').then(function(cache) {
+      caches.open('fs_bst_pdf').then(function(cache) {
         return cache.addAll([
-            "index.html"        
+          // add app with domain
+          "/index.html?appstatus=active",
+          "assets/done_black_24dp.svg",
+          "assets/favicon.png",
+          "assets/ico@192.png",
+          "assets/img_add.svg",
+          "assets/watermark.png",
+          "assets/watermark64",
+          "/docs/index.html",
+          "/APP/libs/cropper.min.css",
+          "/APP/libs/cropper.min.js",
+          "/APP/libs/Google-mt-fonts.woff2",
+          "/APP/libs/jspdf.umd.min.js",
+          "/APP/libs/material.min (1).css",
+          "/APP/libs/material.min.js",
+          "/APP/libs/TeDlert.css",
+          "/APP/libs/TeDlertmain.js",
+          "/APP/pages/create.html",
+          "/APP/pages/settings.html",
+          "/APP/create.js",
+          "/APP/desktop.css",
+          "/APP/style.css",
+          "/APP/ui.js"
         ]);
       })
     );
