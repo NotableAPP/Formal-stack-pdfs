41960098640a1074640526ce4d79254c40156bbb
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-20 18:55:10 +0530

    Update sw.js

--- APP/sw.js
+++ APP/sw.js
@@ -3,28 +3,27 @@ self.addEventListener('install', function(e) {
       caches.open('fs_bst_pdf').then(function(cache) {
         return cache.addAll([
           // add app with domain
-          "/index.html?appstatus=active",
+          "index.html",
           "assets/done_black_24dp.svg",
           "assets/favicon.png",
           "assets/ico@192.png",
           "assets/img_add.svg",
           "assets/watermark.png",
           "assets/watermark64",
-          "/docs/index.html",
-          "/APP/libs/cropper.min.css",
-          "/APP/libs/cropper.min.js",
-          "/APP/libs/Google-mt-fonts.woff2",
-          "/APP/libs/jspdf.umd.min.js",
-          "/APP/libs/material.min (1).css",
-          "/APP/libs/material.min.js",
-          "/APP/libs/TeDlert.css",
-          "/APP/libs/TeDlertmain.js",
-          "/APP/pages/create.html",
-          "/APP/pages/settings.html",
-          "/APP/create.js",
-          "/APP/desktop.css",
-          "/APP/style.css",
-          "/APP/ui.js"
+          "libs/cropper.min.css",
+          "libs/cropper.min.js",
+          "libs/Google-mt-fonts.woff2",
+          "libs/jspdf.umd.min.js",
+          "libs/material.min (1).css",
+          "libs/material.min.js",
+          "libs/TeDlert.css",
+          "libs/TeDlertmain.js",
+          "pages/create.html",
+          "pages/settings.html",
+          "create.js",
+          "desktop.css",
+          "style.css",
+          "ui.js"
         ]);
       })
     );
