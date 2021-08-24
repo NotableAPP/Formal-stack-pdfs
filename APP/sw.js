17e16b41a3bc6e8e983772aae4cb8ff426749060
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-08-24 17:33:15 +0530

    Update sw.js

--- APP/sw.js
+++ APP/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open('fs_bst_pdf_vone').then(function(cache) {
+      caches.open('fs_bst_pdf_vonepointtwo').then(function(cache) {
         return cache.addAll([
           "https://formal-stack.netlify.app/app/",
           "index.html",
